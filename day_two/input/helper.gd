cbc2ff466e21f49c16f1c2d60c7ed716a8acd782
timmno12 <benjamintimm21@gmail.com>
2024-12-04 10:02:31 +0100

    removed inputs

---
+++ day_two/input/helper.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name HELPER
+
+func read_file_to_list(path: String) -> Array:
+	var result: Array = []
+	var file := FileAccess.open(path, FileAccess.READ)
+	
+	if file:
+		# Read the content of the file as text
+		var content: String = file.get_as_text()
+		
+		# Split the content into lines
+		var lines: Array = content.split("\n")
+		
+		# Process each line
+		for line : String in lines:
+			if line.strip_edges() == "": # Skip empty lines
+				continue
+			var splitted = line.split("   ")
+			result.append(splitted)
+	
+	return result
+
+
+func sum(accum, number):
+	return accum + number
