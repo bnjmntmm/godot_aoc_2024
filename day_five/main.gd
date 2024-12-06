b67c0a77c559e608d31f0ea4df64894ab0210b43
timmno12 <benjamintimm21@gmail.com>
2024-12-06 14:32:23 +0100

    day 5 task two done
    not the cleaniest and prettiest but it works

--- day_five/main.gd
+++ day_five/main.gd
@@ -36,12 +36,18 @@ var test_input = "47|53
 var page_ordering_rules = []
 var pages_to_produce = []
 
+var incorrectly_ordered_pages = []
+var newly_ordered_pages = []
+
 var correctly_ordered_pages = []
 var middle_numbers = []
 
 
 func _ready() -> void:
 	helper = HELPER.new()
+	task_two()
+	
+func task_one() -> void:
 	var text = helper.read_file("res://input/input.txt")
 	var split = text.replace("\n", ";")
 	var splitted = split.split(";;")
@@ -60,8 +66,60 @@ func _ready() -> void:
 			entry.append(int(line))
 		pages_to_produce.append(entry)
 	check_the_rules()
-	print(get_middle_page_numbers())
+	#print(get_middle_page_numbers(correctly_ordered_pages))
+		
+		
+		
+func task_two():
+	## For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order.
+	var time_start = Time.get_unix_time_from_system()
+	var text = helper.read_file("res://input/input.txt")
+	###TESTINPUT NEEDS TO BE REPLACED WITH TEXT
+	var split = text.replace("\n", ";")
+	var splitted = split.split(";;")
+	var rules = splitted[0]
+	rules = rules.split(";")
+	var produces = splitted[1]
+	produces = produces.split(";")
+
+	for rule in rules:
+		var seperated = rule.split("|")
+		page_ordering_rules.append([int(seperated[0]),int(seperated[1])])
 	
+	for produce in produces:
+		var entry = []
+		for line: String in produce.split(","):
+			entry.append(int(line))
+		pages_to_produce.append(entry)
+	check_the_rules()
+	
+	for unordered_page in incorrectly_ordered_pages:
+		sort_incorrectly_ordered_pages(unordered_page)
+	var result = get_middle_page_numbers(newly_ordered_pages)
+	var time_end = Time.get_unix_time_from_system()
+	
+	print("Sum of middle numbers: ",result)
+	print("Elapsed Time: ", time_end-time_start)
+	
+
+
+func sort_incorrectly_ordered_pages(array_to_be_sorted: Array) -> void:
+	var sorted = true
+	for rule in page_ordering_rules:
+		var rule_one_index : int = array_to_be_sorted.find(rule[0])
+		var rule_two_index : int = array_to_be_sorted.find(rule[1])
+		if rule_one_index != -1 and rule_two_index != -1 and rule_one_index > rule_two_index:
+			##swap
+			var temp = array_to_be_sorted[rule_one_index]
+			array_to_be_sorted[rule_one_index] = array_to_be_sorted[rule_two_index]
+			array_to_be_sorted[rule_two_index] = temp
+			sorted = false
+	if not sorted:
+		##do it again
+		sort_incorrectly_ordered_pages(array_to_be_sorted)
+	
+	if array_to_be_sorted not in newly_ordered_pages:
+		newly_ordered_pages.append(array_to_be_sorted)
 		
 func check_the_rules() -> void:
 	##check if number of rule[0] is inside line, save the index,
@@ -83,14 +141,19 @@ func check_the_rules() -> void:
 		##if rules are applied and correct -> 
 		if rules_applied:
 			correctly_ordered_pages.append(update_list)
+		else:
+			incorrectly_ordered_pages.append(update_list)
 			
-func get_middle_page_numbers() -> int:
+func get_middle_page_numbers(array: Array) -> int:
 	var sum = 0
-	for pages : Array in correctly_ordered_pages:
+	middle_numbers.clear()
+
+	for pages: Array in array:
 		var middle = int(len(pages) / 2)
 		var numb = pages[middle]
 		middle_numbers.append(numb)
-		#middle_numbers.append(pages[index])
+
 	for number in middle_numbers:
 		sum += number
+
 	return sum
